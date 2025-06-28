9cad4ea2148a2a9ac6d7cbfa4bb2dffd2cf1c997
Redowan Ahmed Siyam <redowanahmed.com@gmail.com>
2025-06-29 01:09:52 +0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,18 @@
+// main.go
+package main
+import (
+    "context"
+    "fmt"
+    "github.com/docker/docker/api/types"
+    "github.com/docker/docker/client"
+)
+
+func main() {
+    cli, err := client.NewClientWithOpts(client.FromEnv)
+    if err != nil { panic(err) }
+    containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+    if err != nil { panic(err) }
+    for _, c := range containers {
+        fmt.Println(c.ID, c.Image)
+    }
+}
